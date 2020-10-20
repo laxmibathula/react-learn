88204182c6bbbd92f5331503be12dae83c5101f5
laxmibathula <laxmi.bathula4@gmail.com>
2020-10-19 20:35:42 -0400

    created a table

---
+++ src/Table/Body/Body.js
@@ -0,0 +1,59 @@
+import React from 'react';
+import './Body.css';
+
+
+function Body(){
+    return(
+        <>
+                   <table className='body'>
+                      <tr> 
+                           <th>Id</th> 
+                           <th>Name</th> 
+                           <th>Address</th> 
+                           <th>Email</th> 
+                           <th>Password</th>
+                    
+                      </tr>
+                      <tr>
+                           <td>1</td>
+                           <td>Jill</td>
+                           <td>567</td>
+                           <td>hjgjhg</td>
+                           <td>78</td>
+                      </tr>
+                      <tr>
+                        <td>2</td>
+                        <td>Eve</td>
+                        <td>Ja6567</td>
+                        <td>9hjgbk4</td>
+                        <td>7879</td>
+                      </tr>
+                      <tr>
+                        <td>3</td>
+                        <td>Eve</td>
+                        <td>Ja6567</td>
+                        <td>9hjgbk4</td>
+                        <td>7879</td>
+                      </tr>
+                      <tr>
+                        <td>4</td>
+                        <td>Eve</td>
+                        <td>Ja6567</td>
+                        <td>9hjgbk4</td>
+                        <td>7879</td>
+                      </tr>
+                      <tr>
+                        <td>5</td>
+                        <td>Eve</td>
+                        <td>Ja6567</td>
+                        <td>9hjgbk4</td>
+                        <td>7879</td>
+                      </tr>
+
+                   </table>
+        </>
+
+    )
+}
+
+export default Body;
---
+++ src/Table/Header/Header.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './Header.css'
+
+function  Table(){
+    return(
+          <>
+          <div className="head">    
+          <div className="inhead"> 
+            <h2 >Welcome to Facebook</h2>
+            {/* <h4 >hi</h4> */}
+            <button>Log Out</button>
+           </div>
+           </div> 
+          </>
+    )
+}
+
+export default Table;
